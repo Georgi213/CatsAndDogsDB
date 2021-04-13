0aa095189e95f96bcb3346f75406ce16aab1ea0c
Georgi213 <77331773+Georgi213@users.noreply.github.com>
2021-04-13 09:21:03 +0300

    Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,44 +34,34 @@ namespace CatsAndDogsDB
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
+            
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(61, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Pets";
-            // 
-            // listBox1
-            // 
+            
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(64, 123);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(194, 160);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
-            // listBox2
-            // 
+            
             this.listBox2.FormattingEnabled = true;
             this.listBox2.Location = new System.Drawing.Point(417, 123);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(194, 160);
             this.listBox2.TabIndex = 2;
-            // 
-            // label2
-            // 
+            
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(414, 55);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Pet Names";
-            // 
-            // Form1
-            // 
+            
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
